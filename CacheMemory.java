6fb07e1f957257d46c5e361bfb75ff92d1580a4f
DaeBOkkk <1semi4304@naver.com>
2025-04-19 19:39:01 +0900

    메모리 클래스 삭제, 램, 캐시메모리 업로드

--- CacheMemory.java
+++ CacheMemory.java
@@ -1,2 +1,43 @@
 public class CacheMemory {
+    int size;
+    int[] addresses;
+    byte[] values;
+    int nextIndext = 0;
+
+    public CacheMemory(int size) {
+        this.size = size;
+        addresses = new int[size];
+        values = new byte[size];
+
+        for(int i = 0; i < size; i++){
+            addresses[i] = -1;
+        }
+    }
+
+    public void write(int address, byte data) {
+        for(int i = 0; i < size; i++){
+            if(addresses[i] == address){
+                values[i] = data;
+                return;
+            }
+        }
+
+        addresses[nextIndext] = address;
+        values[nextIndext] = data;
+        nextIndext = (nextIndext + 1) % size;
+    }
+
+    public byte read(int address) {
+        for(int i = 0; i < size; i++){
+            if(addresses[i] == address){
+                return values[i];
+            }
+        }
+        System.out.println("캐시 미스 발생");
+        return -1;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
--- Ram.java
+++ Ram.java
@@ -1,2 +1,30 @@
 public class Ram {
+    int size;
+    byte[] memoryArray;
+
+    public Ram(int size) {
+        this.size = size;
+        memoryArray = new byte[size];
+    }
+
+    public void write(int address, byte data) {
+        if (address >= 0 && address < size) {
+            memoryArray[address] = data;
+        } else {
+            System.out.println("주소 범위를 벗어났습니다.");
+        }
+    }
+
+    public byte read(int address) {
+        if (address >= 0 && address < size) {
+            return memoryArray[address];
+        } else {
+            System.out.println("주소 범위를 벗어났습니다.");
+            return -1;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
 }
